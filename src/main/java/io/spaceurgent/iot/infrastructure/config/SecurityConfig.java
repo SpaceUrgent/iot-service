b1b82c89a2034ed265598a0c74348bfa34534072
Andrii Pesliak <andrey.pslk@gmail.com>
2026-07-13 22:00:11 +0300

    Add api security.

---
+++ src/main/java/io/spaceurgent/iot/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package io.spaceurgent.iot.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        authorizeRequests -> authorizeRequests.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
